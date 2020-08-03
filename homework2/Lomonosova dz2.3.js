20fff1f8d66c6ced3d6a146f42f14d7570402c23
VictoriyaLomonosova <64746076+VictoriyaLomonosova@users.noreply.github.com>
2020-08-03 17:31:04 +0300

    Delete Lomonosova dz2.3.js

--- homework2/Lomonosova dz2.3.js
+++
@@ -1,7 +0,0 @@
-// 3 задание
-let k = +prompt("Ведите число k");
-let l = +prompt("Ведите число l");
-let m = +prompt("Ведите число m");
-if ((l < k && k < m) || (m < k && k < l)) alert(k);
-if ((k < l && l < m) || (m < l && l < k)) alert(l);
-if ((k < m && m < l) || (l < m && m < k)) alert(m);
