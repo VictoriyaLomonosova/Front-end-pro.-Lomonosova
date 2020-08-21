3391ad2facf77af22bbe230122e1ac194b64f88e
VictoriyaLomonosova <64746076+VictoriyaLomonosova@users.noreply.github.com>
2020-08-21 11:49:26 +0300

    Update 7.3.js

--- homework7/7.3.js
+++ homework7/7.3.js
@@ -1,12 +1,12 @@
-const obj = {};
-
-const str = "aaabbbbcc";
+let obj = {};
+const str = "aaabbc";
 
 function calc(str) {
   for (let i = 0; i < str.length; i++) {
     let l = str[i];
-    obj[l] = isNaN(obj[l]) ? 1 : obj[l] + 1;
+    obj[l] = obj[l] + 1 || 1;
   }
   return obj;
 }
+
 console.log(calc(str));
