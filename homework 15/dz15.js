a080b25b832df7cbe7142a29748e6137497bdca2
VictoriyaLomonosova <64746076+VictoriyaLomonosova@users.noreply.github.com>
2020-10-11 23:29:12 +0300

    Add files via upload

---
+++ homework 15/dz15.js
@@ -0,0 +1,55 @@
+class Clock {
+  constructor({ temp }) {
+    this.temp = temp;
+    this.timer = null;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    hours = hours < 10 ? "0" + hours : hours;
+
+    let mins = date.getMinutes();
+    mins = mins < 10 ? "0" + mins : mins;
+    let secs = date.getSeconds();
+    secs = secs < 10 ? "0" + secs : secs;
+
+    let timeResult = this.temp
+
+      .replace("h", hours)
+      .replace("m", mins)
+      .replace("s", secs);
+    return timeResult;
+  }
+  createDiv() {
+    let timeElement = this.render();
+    for (let i = 0; i < timeElement.length; i++) {
+      var square = document.createElement("span");
+      document.body.appendChild(square);
+    }
+  }
+  addElement() {
+    const timeElement = this.render();
+    for (
+      let i = 0, j = 0;
+      i < timeElement.length, j < document.querySelectorAll("span").length;
+      i++, j++
+    ) {
+      document.querySelectorAll("span")[j].innerText = timeElement[i];
+      //console.log(timeElement);//
+    }
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.timer = setInterval(() => this.addElement(), 1000);
+  }
+}
+
+const clock = new Clock({ temp: "h:m:s" });
+clock.createDiv();
+clock.start();
