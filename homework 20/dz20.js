b17327072938bf26ac0641db97a7c97e0dfebfb9
VictoriyaLomonosova <64746076+VictoriyaLomonosova@users.noreply.github.com>
2020-11-09 19:40:58 +0200

    Add files via upload

---
+++ homework 20/dz20.js
@@ -0,0 +1,96 @@
+const phrasses = [
+  "Как делишки?",
+  "Приятно поговорить с умным человеком!",
+  "У Вас все получится, не сомневайтесь!",
+  "У меня все хорошо, а у Вас?",
+  "Как Вам погодка сегодня?",
+  "Хватит лениться",
+  "Самое время для новых свершений!",
+  "Рада,слышить это",
+  "Привет!",
+  "Я, Алиса",
+  "Нормально",
+  "Вы думаете, что говорите?!",
+  "Хм, чтобы Вам рассказать...",
+  "Почему, Вы разговариваете с ботом?:)",
+  "Ну, ну, не переживайте, Вы так",
+  "Пора задуматся над этим вопросом",
+  "Тогда, вперед, дерзайте!",
+  "Js поможет Вам!",
+  "Да, ну Вас!",
+  "Спасибо",
+  "Круто!",
+  "Прикольно!",
+  "А вот это уже обидно",
+  "Да не за что",
+];
+
+const mainDiv = document.getElementById("main");
+const form = document.querySelector("form");
+const button = document.querySelector("button");
+button.disabled = true;
+const inputField = document.getElementById("input");
+
+function randomValueBot() {
+  min = 0;
+  max = phrasses.length - 1;
+  let result = Math.floor(Math.random() * (max - min + 1)) + min;
+  return phrasses[result];
+}
+function timeDelay(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
+function scrollToLastMessage() {
+  const lastMessageInChat = mainDiv.lastChild;
+  console.log(lastMessageInChat);
+  lastMessageInChat.scrollIntoView({
+    block: "center",
+    behavior: "smooth",
+  });
+}
+function addChat(input) {
+  const userDiv = document.createElement("div");
+  userDiv.classList = "user";
+
+  userDiv.innerHTML = `Вы: ${input}`;
+
+  mainDiv.appendChild(userDiv);
+  scrollToLastMessage();
+}
+
+async function botAnswer() {
+  const botDiv = document.createElement("div");
+  botDiv.classList = "bot";
+
+  if (inputField.value !== "Пока!") {
+    await timeDelay(3000);
+    botDiv.innerHTML = `Алиса: ${randomValueBot()}`;
+    mainDiv.appendChild(botDiv);
+  } else {
+    await timeDelay(2000);
+    botDiv.innerHTML = `Алиса:Пока!Еще увидимся`;
+
+    mainDiv.appendChild(botDiv);
+    inputField.setAttribute("disabled", "disabled");
+  }
+  scrollToLastMessage();
+}
+
+form.addEventListener("submit", function (e) {
+  let input = inputField.value;
+  if (input !== "") output(input);
+  e.preventDefault();
+  inputField.value = "";
+
+  button.disabled = true;
+});
+
+inputField.addEventListener("input", (event) => {
+  const value = event.target.value;
+  button.disabled = value === "";
+});
+
+function output(input) {
+  addChat(input);
+  botAnswer();
+}
